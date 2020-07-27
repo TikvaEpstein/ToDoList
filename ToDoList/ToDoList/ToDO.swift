b6614be75c399ea50d98b30d560042a1e414df87
TikvaEpstein <68654805+TikvaEpstein@users.noreply.github.com>
2020-07-27 14:40:35 -0400

    Iteration 1

---
+++ ToDoList/ToDoList/ToDO.swift
@@ -0,0 +1,14 @@
+//
+//  ToDO.swift
+//  ToDoList
+//
+//  Created by Tikva on 7/27/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = " "
+    var important = false 
+}
